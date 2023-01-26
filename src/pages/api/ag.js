03cbf9529d3a47de6e36d65a93698f3f1485aea1
martialdwarf <mohammadfahadamajd@gmail.com>
2023-01-26 16:30:59 +0500

    first

---
+++ src/pages/api/ag.js
@@ -0,0 +1,45 @@
+import edgeChromium from 'chrome-aws-lambda';
+
+import puppeteer from 'puppeteer-core';
+
+export default async function handler(req, res) {
+
+  const executablePath = await edgeChromium.executablePath;
+
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  });
+
+  await page.goto('http://lms.uaf.edu.pk/login/index.php')
+
+  await page.type('#REG', '2020-ag-8322');
+
+  await Promise.all([
+    page.click('input[value=Result]'),
+    page.waitForNavigation(),
+  ]);
+
+  let data = await page.evaluate(() => {
+    let values = []
+    let tableRows = document.querySelectorAll('tr')
+
+    tableRows.forEach((row) => {
+      let children = {}
+      let index = 0
+      row.childNodes.forEach((child) => {
+        if (child.innerText != null) {
+          children[index] = child.innerText
+          index++
+        }
+      })
+      values.push(children)
+    })
+
+    return values
+  });
+
+
+  res.status(200).json({data: data});
+}
